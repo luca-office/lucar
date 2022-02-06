1be965e49fc60f2a9b7f71bb89ba904d22a35ab7
Steffen Brandt <steffen74@users.noreply.github.com>
2022-02-06 22:27:37 +0100

    bug fix and first version with fully constructed workflow table (still missing completion of various workflow codes

--- R/get_workflow.R
+++ R/get_workflow.R
@@ -16,8 +16,8 @@ get_workflow <- function (json_data) {
   }
 
   # formatting the events into a tibble
-  events <- as_tibble(data.frame(matrix(unlist(json_data$surveyEvents, recursive=FALSE), nrow=length(events), byrow=TRUE))) %>% # transforming nested list into a iibble
-    rename_with(~names(events[[1]])) %>% # retrieving original column names
+  events <- as_tibble(data.frame(matrix(unlist(json_data$surveyEvents, recursive=FALSE), nrow=length(json_data$surveyEvents), byrow=TRUE))) %>% # transforming nested list into a iibble
+    rename_with(~names(json_data$surveyEvents[[1]])) %>% # retrieving original column names
     mutate(across(c(timestamp, eventType, index), ~sapply(.x, function(x) x[[1]]))) # unnest columns provided in lists
 
   # get all project elements and their respective workflow codes
@@ -25,33 +25,14 @@ get_workflow <- function (json_data) {
 
   workflow <- left_join(events, basic_wf_codes, by="eventType") %>%
     select(invitation_id=invitationId, survey_id=surveyId, timestamp, label, eventType, wf_code, data=data.x, index=index.x) %>%
-    mutate(time=lubridate::as_datetime(timestamp[[1]]) )%>%
+    mutate(time=lubridate::as_datetime(timestamp) )%>%
     tidyr::unnest_wider(data) %>%
     left_join(project_elements, by="id") %>%
     mutate(wf_code.y=replace(wf_code.y, is.na(wf_code.y), "")) %>%
     mutate(wf_code=paste0(wf_code.x, wf_code.y)) %>%
-    mutate(scenario_time = as.difftime("")) %>%
-    mutate(event_duration = as.difftime("")) %>%
+    mutate(scenario_time = time-time[1]) %>%
+    mutate(event_duration = time-lag(time)) %>%
     select(invitation_id, surveyId, scenario_id=scenarioId, time, scenario_time, event_duration, label, event_type=eventType, wf_code, tool, name, usage_type)
-
-  for (i in 1:nrow(workflow)) {
-    event <- workflow[i,]
-
-    if (is.null(workflow)) {
-      # in case this event is the first in the considered workflow, set some further initial values and use current workflow_event to initialize the workflow table
-      workflow_event$scenario_time <- as.difftime("0:0:0")
-      workflow <- workflow_event
-    } else {
-      # calculating the duration of the event before in the workflow (currently the last one)
-      event_before <- nrow(workflow)
-      workflow[event_before,]$event_duration <- workflow_event$time - workflow[event_before,]$time
-      # calculating the time since the start of the scenarion
-      workflow_event$scenario_time <- workflow_event$time - workflow[1,]$time
-      # adding event data to already existing events in the workflow
-      workflow <- rbind(workflow, workflow_event)
-    }
-
-  }
 
   return(workflow)
 }
