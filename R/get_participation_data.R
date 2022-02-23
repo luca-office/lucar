a1a8d954fe17777079ca4fbb9293f86e27cc9806
Steffen Brandt <steffen74@users.noreply.github.com>
2022-02-23 16:58:29 +0100

    version 0.0.4 - particpation data now includes workflow data - scenario-specific preparation was removed as argument from function prepare_logdata

--- R/get_participation_data.R
+++ R/get_participation_data.R
@@ -5,7 +5,7 @@
 #' @param json_data The log data for a single participation in form of a json object
 #' @param hash_ids If TRUE the internal hash IDs for the projct elements are included
 #'
-#' @return A dataframe (consisting one row) including general information for a single participation
+#' @return A dataframe (consisting of one row) including general information for a single participation
 #'
 #' @examples
 #' \dontrun{
@@ -14,30 +14,31 @@
 #' participation_data <- get_participation_data(json_data)
 #' }
 #'
+#' @importFrom dplyr tibble
 #' @export
 get_participation_data <- function (json_data, hash_ids=FALSE){
 
     # Initialization of the dataframe row for the general answer data of the participation with the participant ID
-    participation <- data.frame(id = json_data$surveyInvitation$id)
-
-    # extract basic project and survey info
-    participation$project <- json_data$project$title
-    participation$survey <- json_data$survey$title
-
-    # extract basic participation info
-    participation$token <- json_data$surveyInvitation$token
-    participation$account_participation <- json_data$surveyInvitation$isUserAccountParticipation
-    participation$open_participation <- json_data$surveyInvitation$isOpenParticipation
-    participation$project_start <- getTime(json_data$surveyInvitation$firstSurveyEventTimestamp)
-    participation$project_end <- getTime(json_data$surveyInvitation$lastSurveyEventTimestamp)
-    if (length(json_data$surveyEvents)>0) {
-      participation$salutation <- json_data$surveyEvents[[1]]$data$salutation
-      participation$first_name <- json_data$surveyEvents[[1]]$data$firstName
-      participation$last_name <- json_data$surveyEvents[[1]]$data$lastName
-    }
-    participation <- participation %>%
+    participation <- tibble(id = json_data$surveyInvitation$id) %>%
+      # extract basic project and survey info
+      mutate(project = json_data$project$title, survey = json_data$survey$title) %>%
+      # extract basic participation info
+      mutate(token = json_data$surveyInvitation$token,
+             account_participation = json_data$surveyInvitation$isUserAccountParticipation,
+             open_participation = json_data$surveyInvitation$isOpenParticipation,
+             did_participate = as.logical(length(json_data$surveyEvents)>0),
+             project_start = getTime(json_data$surveyInvitation$firstSurveyEventTimestamp),
+             project_end = getTime(json_data$surveyInvitation$lastSurveyEventTimestamp)) %>%
+      # Remove ID variable if indicated by boolean 'hash_ids'
       dplyr::select_if(hash_ids|!grepl("^id$|Id$", names(.)))
 
+    # checking if invited particpant actually participated
+    if (participation$did_participate) {
+      participation <- participation %>%
+        mutate(salutation = json_data$surveyEvents[[1]]$data$salutation,
+               first_name = json_data$surveyEvents[[1]]$data$firstName,
+               last_name <- json_data$surveyEvents[[1]]$data$lastName)
+    }
 
   return(participation)
 
--- R/prepare_logdata.R
+++ R/prepare_logdata.R
@@ -7,7 +7,6 @@
 #'
 #' @param path The path to the folder including all JSON files (files in subfolders are also considered).
 #' @param unzip If true, the function looks for zip archives located in the given path, corresponding to the naming convention for exported data from LUCA Office, and unzips these.
-#' @param scenario_specific If TRUE the workflow is split into separate lists for each scenario element
 #' @param workflow_codes Dataframe with the workflow coding that is used to structure the log data
 #' @param tool_codes Dataframe with the tool coding that is used to assign each used tool to a common code
 #' @param debug_mode If TRUE the results include the internal hash IDs for the project elements are included and a tibble including unknown event types (if there were any). If 'scenario_specific' is set to TRUE it will be enforced to 'FALSE'.
@@ -25,17 +24,17 @@
 #' @importFrom rjson fromJSON
 #' @importFrom dplyr tibble
 #' @export
-prepare_logdata <- function (path = "./", unzip = FALSE, scenario_specific=TRUE,
-                             workflow_codes=lucar::workflow_coding, tool_codes=lucar::tool_coding,
-                             debug_mode=FALSE){
+prepare_logdata <- function (path = "./", unzip = FALSE, workflow_codes=lucar::workflow_coding,
+                             tool_codes=lucar::tool_coding, debug_mode=FALSE){
 
-  # Forcing 'scenario_specific' to TRUE for debigging mode
-  if (debug_mode & scenario_specific) {
+  # Setting 'scenario_specific' workflow preparation to FALSE for debugging mode and TRUE otherwise
+  if (debug_mode) {
     scenario_specific <- FALSE
-    warning("Argument 'scenario_specific' is enforced to FALSE since 'debug_mode' was set TRUE.")
+  } else {
+    scenario_specific <- TRUE
   }
 
-  # unzip files if necessary
+  # unzip files if indicated by function argument
   if (unzip){
     zip_files <- grep("^Luca-Erhebungsdaten-.*\\.zip$", list.files(path), value=TRUE)
     for (zip_file in zip_files){
@@ -64,26 +63,33 @@ prepare_logdata <- function (path = "./", unzip = FALSE, scenario_specific=TRUE,
     element_name <- sub('\\..*$', '', basename(json_file))
     workflows[[element_name]] <- get_workflow(json_data, scenario_specific=scenario_specific, workflow_codes, tool_codes, hash_ids=debug_mode)
 
-    # in debugging mode: collects incomplete wf_codes, e.g. due to unknown events or unmatched id's
-    if (debug_mode) {
+    # construct new tibble row for the tibble including the participation data on all participants
+    new_participation <- get_participation_data(json_data, debug_mode)
+    if (!debug_mode) {
+      # if not debugging mode: add summary information on the specific scenario workflows
+      for (scenario in names(workflows[[element_name]])) {
+        new_participation[[paste0("duration_scenario_",scenario)]] <- workflows[[element_name]][[scenario]]$scenario_time[length(workflows[[element_name]][[scenario]]$scenario_time)]
+        new_participation[[paste0("wf_codes_scenario_",scenario)]] <- list(workflows[[element_name]][[scenario]]$wf_code)
+        new_participation[[paste0("event_durations_",scenario)]] <- list(workflows[[element_name]][[scenario]]$event_duration)
+      }
+    } else {
+      # in debugging mode: collect incomplete wf_codes, e.g. due to unknown events or unmatched id's
       incomplete_codes <- nchar(workflows[[element_name]]$wf_code)!=10
       unknown_events <- rbind(unknown_events, workflows[[element_name]][incomplete_codes,])
       unknown_events <- unknown_events[!duplicated(unknown_events$event_type),]
     }
-
-
-    # add new dataframe row for the given participation
-    new_participation <- get_participation_data(json_data, debug_mode)
+    # add new participant data to the already existing one
     if (is.null(participation)){
       participation <- new_participation
     } else {
       participation <- rbind(participation, new_participation)
     }
 
+
   }
 
   if (is.null(participation)){
-    warning("Neither the folder nor the subfolders of the given path include a JSON file!")
+    stop("Neither the folder nor the subfolders of the given path include a JSON file!")
   }
 
   # add dataframe including unknown events if indicated
